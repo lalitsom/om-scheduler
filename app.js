4368390af10d47fc92c5cbbc3b345872fe523c6d
lalitsom27 <lalitsom27@gmail.com>
2020-06-30 07:31:52 +0530

    asd

---
+++ app.js
@@ -0,0 +1,91 @@
+var fs = require('fs');
+var http = require('http');
+const { start } = require('repl');
+
+destPath = "./serve/murli_audio.mp3";
+srcPath = "http://www.babamurli.com/01.%20Daily%20Murli/01.%20Hindi/10.%20Hindi%20Murli%20-%20Mumbai/datePlaceHolder.mp3";
+
+// functions
+startSync = () => {
+    if (alreadyDownloadedToday(destPath)) {
+        log("No need to download and update... exiting");
+        return;
+    }
+    try {
+        downloadAndUpdate();
+    } catch (err) {
+        log("error " + err);
+    }
+}
+
+
+downloadAndUpdate = () => {
+    src = getAudioSource(srcPath);
+    downloadFile(src, destPath);
+}
+
+
+downloadFile = (src, dest) => {
+    var file = fs.createWriteStream(dest);
+    console.log("starting");
+    var request = http.get(src, function (response) {
+        response.pipe(file);
+        response.on('end', function () {
+            file.end();
+            log("Download completed");
+            if (alreadyDownloadedToday(destPath)) {
+                updateGit();
+            } else {
+                log("error, not downloaded ");
+            }
+
+        });
+    });
+
+    return true;
+}
+
+updateGit = () => {
+    log("updating git...")
+}
+
+
+getAudioSource = (baseUrl) => {
+    return baseUrl.replace("datePlaceHolder", getDate());
+}
+
+alreadyDownloadedToday = (path) => {
+    try {
+        if (fs.existsSync(path)) {
+            const stats = fs.statSync(path);
+            if(new Date().getDate() == stats.mtime.getDate()){
+                return true;
+            }
+        }
+    } catch (err) {
+        log("error " + err);
+        return false;
+    }
+}
+
+log = (text) => {
+    console.log(text);
+}
+
+getDate = () => {
+    var today = new Date();
+    date = today.getDate();
+    month = today.getMonth() + 1;
+    year = today.getFullYear();
+
+    if (date.toString().length == 1) {
+        date = "0" + String(date);
+    }
+    if (month.toString().length == 1) {
+        month = "0" + String(month);
+    }
+    return String(date) + "." + String(month) + "." + String(year).substr(2, 2);
+}
+
+
+startSync();
